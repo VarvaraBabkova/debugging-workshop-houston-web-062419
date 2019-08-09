a331c92cae6b8fa220974423f9d6e07bd295fca1
Varvara Babkova <varvara.munday@gmail.com>
2019-08-09 16:35:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
   const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
-  let joke;
+  let joke = "";
+  let username = "";
 
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+    console.log("in fetch")
+     fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
     })
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
-  }
+    .then(jokeData => {
+       joke = jokeData.joke
+      username = document.getElementById('name-input').value
 
-  form.addEventListener('submit', (event) => {
-
-    if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
+       newJokeLi.innerHTML = `
     <span class="username">${username} says:</span> ${joke}
     `
     jokeList.appendChild(newJokeLi)
+       
+    })
+  }
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    username = document.getElementById('name-input').value
+
+    if(username === "") return;
+    fetchJoke()
+    
+    
   })
 })
